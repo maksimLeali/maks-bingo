87610cd1c73d4efee1a1f0d4d308580f25c2392b
Maksim - Gotham Studio <maksim-gothamstudio@Mac-mini.local>
2024-02-13 16:52:57 +0100

    replaced uslesss import

--- src/utils/themeFunctions.ts
+++ src/utils/themeFunctions.ts
@@ -1,8 +1,9 @@
-import { DefaultTheme } from "styled-components";
 
-export const $uw = (uw: number) => ({ theme }: { theme: DefaultTheme }) => theme.uw(uw);
-export const $color = (color: string) => ({ theme }: { theme: DefaultTheme }) => theme.colors[color]
+type Theme =  {[key: string]: any} 
+
+export const $uw = (uw: number) => ({ theme }: { theme: Theme }) => theme.uw(uw);
+export const $color = (color: string) => ({ theme }: { theme: Theme }) => theme.colors[color]
 
 export const $padding = (top: number, right: number = top, bottom: number = top, left: number = right) => {
-    return ({ theme }: { theme: DefaultTheme }) => `${theme.uw(top)} ${theme.uw(right)} ${theme.uw(bottom)} ${theme.uw(left)}`
+    return ({ theme }: { theme: Theme }) => `${theme.uw(top)} ${theme.uw(right)} ${theme.uw(bottom)} ${theme.uw(left)}`
 }
